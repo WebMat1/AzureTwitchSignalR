5a4d7c0f5d4e5391c218b40aeea0d2dc25cbc5cb
Mateus Faria <webmat1@hotmail.com>
2020-11-13 15:55:46 -0300

    Right architecture

---
+++ BlazorTwitch/Client/bin/Release/netstandard2.1/wwwroot/service-worker-assets.js
@@ -0,0 +1,269 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-BHNhq10T\/9aejlrFEhS\/NYeYS3Djwv\/pqg2xUjQCx\/s=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-rldnE7wZYJj3Q43t5v8fg1ojKRwyt0Wtfm+224CacZs=",
+      "url": "css\/bootstrap\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-xMZ0SaSBYZSHVjFdZTAT\/IjRExRIxSriWcJLcA9nkj0=",
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-jA4J4h\/k76zVxbFKEaWwFKJccmO0voOQ1DbUW+5YNlI=",
+      "url": "css\/open-iconic\/FONT-LICENSE"
+    },
+    {
+      "hash": "sha256-BJ\/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=",
+      "url": "css\/open-iconic\/font\/css\/open-iconic-bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-OK3poGPgzKI2NzNgP07XMbJa3Dz6USoUh\/chSkSvQpc=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.eot"
+    },
+    {
+      "hash": "sha256-sDUtuZAEzWZyv\/U1xl\/9D3ehyU69JE+FvAcu5HQ+\/a0=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.otf"
+    },
+    {
+      "hash": "sha256-+P1oQ5jPzOVJGC52E1oxGXIXxxCyMlqy6A9cNxGYzVk=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.svg"
+    },
+    {
+      "hash": "sha256-p+RP8CV3vRK1YbIkNzq3rPo1jyETPnR07ULb+HVYL8w=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.ttf"
+    },
+    {
+      "hash": "sha256-cZPqVlRJfSNW0KaQ4+UPOXZ\/v\/QzXlejRDwUNdZIofI=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.woff"
+    },
+    {
+      "hash": "sha256-aF5g\/izareSj02F3MPSoTGNbcMBl9nmZKDe04zjU\/ss=",
+      "url": "css\/open-iconic\/ICON-LICENSE"
+    },
+    {
+      "hash": "sha256-p\/oxU91iBE+uaDr3kYOyZPuulf4YcPAMNIz6PRA\/tb0=",
+      "url": "css\/open-iconic\/README.md"
+    },
+    {
+      "hash": "sha256-qU+KhVPK6oQw3UyjzAHU4xjRmCj3TLZUU\/+39dni9E0=",
+      "url": "favicon.ico"
+    },
+    {
+      "hash": "sha256-IpHFcQvs03lh6W54zoeJRG1jW+VnGmwymzxLoXtKX7Y=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-sMgQxZm0by9+wHYQFoWphkEOazKlg99zhDN+Pbkm5F8=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-EOh4K9udzSu5sLVQERA3EyJHAJWNSbifEGPJz\/1yMTM=",
+      "url": "manifest.json"
+    },
+    {
+      "hash": "sha256-b0gCm+NDRhgHQkcitJDpi2YeuULXjhWgO\/1zkLUfK3A=",
+      "url": "_framework\/_bin\/BlazorTwitch.Client.dll"
+    },
+    {
+      "hash": "sha256-9naDHUlAeQ3eF55z58e\/+WlVB076hPvthz6iJ2RXuXc=",
+      "url": "_framework\/_bin\/BlazorTwitch.Shared.dll"
+    },
+    {
+      "hash": "sha256-yUPZcKgbAkxqpEwRBG2qt1vWGxykRk9pLpflS+8V+BI=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.dll"
+    },
+    {
+      "hash": "sha256-Rz7C1oFfS1Wx25u9z4rLPUkJ7WOi9LF08H60oMUz5vI=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.Web.dll"
+    },
+    {
+      "hash": "sha256-nUD3mStQZWtbol\/GC4CnmBnnE6QqiVqjEeFMK6F8Yas=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-7deMAlPIkIEWDqRRKjkqM0tao\/eJ7peKmYzwWygSbe4=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Connections.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-2bg6aK4em5N5TsfZrvLMr5iSGGTQ49xzdiyfQMlLEqw=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Http.Connections.Client.dll"
+    },
+    {
+      "hash": "sha256-d2rcdbVIKGuzlJkBWVKcZ2FSaqxYjoqBTdLWVjUlueI=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Http.Connections.Common.dll"
+    },
+    {
+      "hash": "sha256-JItHargjFdO6DJ3lFoTgoOEa2tn9LiIQLybW9HYQ2Lo=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Http.Features.dll"
+    },
+    {
+      "hash": "sha256-4C34ksIQuAQ1gmmNhpUTs+ipzlTt2ByQejqtPL5fjU8=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.SignalR.Client.Core.dll"
+    },
+    {
+      "hash": "sha256-v0JBwYfJEGHGxzbBPM7IImYg49ibsghCLVhZ86M5xrQ=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.SignalR.Client.dll"
+    },
+    {
+      "hash": "sha256-qFGuPS76YT5Dfcy4mq6BWrt+kcg2siiOFCbSTleXKAQ=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.SignalR.Common.dll"
+    },
+    {
+      "hash": "sha256-R50uXtkEm2X\/I5DIO04I9oVgorMtbRCer1WPZpS50Xs=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.SignalR.Protocols.Json.dll"
+    },
+    {
+      "hash": "sha256-jzHgXWAvWMkKIGFjZoT84tbe72E+H7CvTr\/Dryh4QPs=",
+      "url": "_framework\/_bin\/Microsoft.Bcl.AsyncInterfaces.dll"
+    },
+    {
+      "hash": "sha256-3ytty1YLNe0+uNtz\/tKierD3sOVTIi9BBTv1n9srxxM=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-jDgdr4duMb2wAsWbAeLOGFD3h0+ESJS9wXiQc2oASOI=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.dll"
+    },
+    {
+      "hash": "sha256-FdodG8X0dwmCv0MyNXh+dwvTPRHkWfnSSZpU09WdpbM=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Json.dll"
+    },
+    {
+      "hash": "sha256-VWH\/397gNbMD6V66r8+KA4bVpaPOADmOlX\/tFRLM1SQ=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-Ap6nuR+ltA2oi63OlfLedayOgs\/c3hEel0EDjTFF864=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.DependencyInjection.dll"
+    },
+    {
+      "hash": "sha256-4\/+F9SN0LgWlNA\/rWfFePP5o9VnGyCvMG6QMy5F\/4RM=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Logging.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-NrWILWwYI5cFMg8B2YOfi5XnqZMIx57em4Xi\/mN7m7k=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Logging.dll"
+    },
+    {
+      "hash": "sha256-GrzBeljJgK7CD1uHtce4YK0e742qQ6YcOzE56NRTnd8=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Options.dll"
+    },
+    {
+      "hash": "sha256-+INHA5vsV2Yh\/AM4zThzaApKfaguVWrO1CI9tH0B2NU=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Primitives.dll"
+    },
+    {
+      "hash": "sha256-3cbvJEo8k8VJVQwyEZWWQ6yGRFQW8\/219DaOnmsX3e4=",
+      "url": "_framework\/_bin\/Microsoft.JSInterop.dll"
+    },
+    {
+      "hash": "sha256-zV\/OfJNfNsG404X30Pwkdymdh1KmZpTVEmG\/JNFVc\/k=",
+      "url": "_framework\/_bin\/Microsoft.JSInterop.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-ivU3mJ1FBH6bZCzuTZmb1lXfXVGQtFpRQs6cuO9Y35k=",
+      "url": "_framework\/_bin\/Mono.Security.dll"
+    },
+    {
+      "hash": "sha256-pp00IQsh2ZoZQ0AuDWWVXEdz8xL7XvgDpBF37sPC1GM=",
+      "url": "_framework\/_bin\/mscorlib.dll"
+    },
+    {
+      "hash": "sha256-8+aWlQshkso8ei8xp5J0gn4ZNGRYRMqXzI+osKzgVYI=",
+      "url": "_framework\/_bin\/Newtonsoft.Json.dll"
+    },
+    {
+      "hash": "sha256-gpmYNFgYSm\/miPDvKlEyvMY2a3n8GKgGymaci6gZKuU=",
+      "url": "_framework\/_bin\/System.Core.dll"
+    },
+    {
+      "hash": "sha256-\/LDgZP6f+awaOEDtNIFSay\/gxN3hqSacwxtCVKfz5+w=",
+      "url": "_framework\/_bin\/System.Data.dll"
+    },
+    {
+      "hash": "sha256-XLlhtqJc3dM7suq+YTSF6Bc1OWc5jpTdz0K\/EO4iAkM=",
+      "url": "_framework\/_bin\/System.dll"
+    },
+    {
+      "hash": "sha256-KgbuyhMH+Tgoa0ohQchYO+\/2J+xnGe1IIuPSR715gH4=",
+      "url": "_framework\/_bin\/System.IO.Pipelines.dll"
+    },
+    {
+      "hash": "sha256-wJx\/+OmRYhC5ZcJw+PUo7GRWIQfPmV\/u\/SXWRVAygcY=",
+      "url": "_framework\/_bin\/System.Net.Http.dll"
+    },
+    {
+      "hash": "sha256-fYswCo4BtH\/XNHnCtzRy3mTRhUSJ0tCeQcSFQF7BrCw=",
+      "url": "_framework\/_bin\/System.Net.Http.Json.dll"
+    },
+    {
+      "hash": "sha256-gpw1GfB05jwlYcs\/OBjRcrpF6UiY\/ejhYXCs40LbuRQ=",
+      "url": "_framework\/_bin\/System.Net.Http.WebAssemblyHttpHandler.dll"
+    },
+    {
+      "hash": "sha256-2kUeb995ChJNfMNHuCHacpHQ5fWKuHI2LvNyLsLfN\/w=",
+      "url": "_framework\/_bin\/System.Numerics.dll"
+    },
+    {
+      "hash": "sha256-bzzohifFrJaN4bRqg2dnoYR28xBC5mZjFp07J7VZ7IE=",
+      "url": "_framework\/_bin\/System.Runtime.CompilerServices.Unsafe.dll"
+    },
+    {
+      "hash": "sha256-xEQXr7Uo\/PiIIg4QHLeCLTwXIy7M85yXKzsBeI8JeVc=",
+      "url": "_framework\/_bin\/System.Runtime.Serialization.dll"
+    },
+    {
+      "hash": "sha256-zsCnjnSjw0jGZM\/NBfhNIdJrKQ5rf0VagLMPK45sIU0=",
+      "url": "_framework\/_bin\/System.Text.Encodings.Web.dll"
+    },
+    {
+      "hash": "sha256-p1w81rlVgivkzIt74atetdqLYa6F8C4y9SnZmh+Efiw=",
+      "url": "_framework\/_bin\/System.Text.Json.dll"
+    },
+    {
+      "hash": "sha256-FCYlJ8UOzQbtFg+kBSJR4OBz7yeFjNzzXqkmzXCGJhU=",
+      "url": "_framework\/_bin\/System.Threading.Channels.dll"
+    },
+    {
+      "hash": "sha256-6l\/oKzhty+4sZL4RSwUwXKzYAhbXe70dsP1uZSfR42w=",
+      "url": "_framework\/_bin\/System.Xml.dll"
+    },
+    {
+      "hash": "sha256-ZvTG+\/Ed2OwprsWyBaXZ1gRP9+2PdGAWj8Eah5w+EgU=",
+      "url": "_framework\/_bin\/System.Xml.Linq.dll"
+    },
+    {
+      "hash": "sha256-ulbyQf0RTB9Ymcf7gNkR39lVw+CHoFlDqZWdDC2Ttnw=",
+      "url": "_framework\/_bin\/WebAssembly.Bindings.dll"
+    },
+    {
+      "hash": "sha256-1srhUVGIzpo+hMZmvo\/NNnLZdtTvs5wO7IXK0kUF\/JE=",
+      "url": "_framework\/_bin\/WebAssembly.Net.WebSockets.dll"
+    },
+    {
+      "hash": "sha256-mPoqx7XczFHBWk3gRNn0hc9ekG1OvkKY4XiKRY5Mj5U=",
+      "url": "_framework\/wasm\/dotnet.3.2.0.js"
+    },
+    {
+      "hash": "sha256-3S0qzYaBEKOBXarzVLNzNAFXlwJr6nI3lFlYUpQTPH8=",
+      "url": "_framework\/wasm\/dotnet.timezones.dat"
+    },
+    {
+      "hash": "sha256-UC\/3Rm1NkdNdlIrzYARo+dO\/HDlS5mhPxo0IQv7kma8=",
+      "url": "_framework\/wasm\/dotnet.wasm"
+    },
+    {
+      "hash": "sha256-SPHS1EAPAcMFN4TEIUYl3FWtoCQTsNJch0XVGgok1eE=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-PweyIMh0u4uE\/RUfbMB7JE8QZkYxfhAGaCxLQvulOdk=",
+      "url": "_framework\/blazor.boot.json"
+    }
+  ],
+  "version": "EQC+J945"
+};
---
+++ ClientTeste/Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Net.Http.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace ClientTeste
+{
+    class Program
+    {
+        public static List<object> messages = new List<object>();
+        //url de conexão
+        public static string url = "https://azuretwitchsignalr.azurewebsites.net/api";
+        //public static string url = "http://localhost:7071/api/";
+
+        public static int id = (new Random()).Next();
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var firstInput = Console.ReadLine();
+
+                switch (firstInput)
+                {
+                    case "0":
+                        await testClientAsync();
+                        break;
+                }
+
+                // declara variaveis 
+                HubConnection connection;
+
+                //passa as diretrizes da conexão 
+                connection = new HubConnectionBuilder().WithUrl(url).WithAutomaticReconnect().Build();
+
+                // faz os contratos para receber as info em tempo real 
+
+                //contrato para receber quando o servidor fizer logout deste cliente 
+                connection.On<object>("newMessage", (msg) => newMessage(msg));
+
+                //inicia a conexão com o servidor 
+                await connection.StartAsync();
+
+                while (true)
+                {
+                    sendMessage(Console.ReadLine());
+                }
+            }
+            catch (Exception except)
+            {
+                Console.WriteLine(except.Message + "\t\r\n");
+            }
+        }
+
+        public static void newMessage(object message)
+        {
+            Console.Clear();
+
+            var obj = new { sender = "", text = "" };
+
+            obj = Newtonsoft.Json.JsonConvert.DeserializeAnonymousType(message.ToString(), obj);
+
+            messages.Add(message);
+
+            foreach (object item in messages)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        public static async Task testClientAsync()
+        {
+            HttpClient http = new HttpClient();
+
+            var response = await http.GetAsync(url + "/Function?name=MatWeb");
+        }
+
+        public static async void sendMessage(string msg)
+        {
+            HttpClient http = new HttpClient();
+
+            var response = await http.PostAsJsonAsync(url + "/messages", new { sender = id.ToString(), text = "id enviou"+msg });
+        }
+
+    }
+}
